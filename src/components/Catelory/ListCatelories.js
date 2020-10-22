35c8a1ddb627870e58b01721e3de06fc55bfc873
unknown <phuonghoi1205@gmail.com>
2020-10-22 16:07:20 +0700

    list category

--- src/components/Catelory/ListCatelories.js
+++ src/components/Catelory/ListCatelories.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import Axios  from 'axios';
 
 class ListCatelories extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            listCategories :[]
+        }
+    }
+    componentDidMount() {
+        Axios.get('http://127.0.0.1:8000/api/categories').then((response) => {
+            this.setState({
+                listCategories: response.data
+            })
+        })
+    }
+    
     render() {
+        const listCategories = this.state.listCategories.map((items,index)=>{
+            return <Category stt={index+1} categories={items} key={index} />
+        })
         return (
             <div className="content-wrapper">
                 <div className="main-list">
@@ -18,87 +36,16 @@ class ListCatelories extends Component {
                                 <tr>
                                     <th>STT</th>
                                     <th>Tên chuyên mục</th>
-                                    <th>Chuyên mục cha</th>
+                                    <th>Trạng Thái</th>
                                     <th>Hành động</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">1</td>
-                                    <td>Anime</td>
-                                    <td>Không có</td>
-                                    <td>
-                                        <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
-                                        <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
-                                    </td>
-                                </tr>
+                                {listCategories}
                             </tbody>
                         </table>
                     </div>
-                    <div className="num-record">(Có 10 bản ghi)</div>
+                    <div className="num-record">(Có {this.state.listCategories.length} bản ghi)</div>
                     <div className="paging">
                         <ul id="list-paging" className="fl-right">
                             <li>
@@ -127,5 +74,24 @@ class ListCatelories extends Component {
         );
     }
 }
+
+
+class Category extends Component {
+    render() {
+        var Ctgr = this.props.categories;
+        return (
+            <tr>
+                <td scope="row">{this.props.stt}</td>
+                <td>{Ctgr.name}</td>
+                <td>{Ctgr.description}</td>
+                <td>
+                    <a href title="Sửa" className="edit"><i className="fa fa-pencil icon" /></a>
+                    <a href title="Xóa" className="delete"><i className="fa fa-trash icon" /></a>
+                </td>
+            </tr>
+        );
+    }
+}
+
 
 export default ListCatelories;
